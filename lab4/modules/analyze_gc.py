858881aef4a633dbcd2754e79f2e29b8f6b3fdda
alzhbeta <elizabethbaliuk@gmail.com>
2026-05-28 01:26:11 +0300

    Update analyze_gc.py

--- lab4/modules/analyze_gc.py
+++ lab4/modules/analyze_gc.py
@@ -1,1 +1,9 @@
+from Bio.SeqUtils import gc_fraction
 
+def sort_by_gc(records):
+    sorted_records = []
+    for r in records:
+        gc_val = gc_fraction(r.seq)
+        sorted_records.append((gc_val, r))
+    sorted_records.sort(key=lambda x: x[0])
+    return sorted_records
