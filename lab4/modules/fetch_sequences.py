11ae47792fa49e451adc5cc52b8f033507a9b358
alzhbeta <elizabethbaliuk@gmail.com>
2026-05-28 01:24:54 +0300

    Update fetch_sequences.py

--- lab4/modules/fetch_sequences.py
+++ lab4/modules/fetch_sequences.py
@@ -1,1 +1,7 @@
+import os
+from Bio import SeqIO
 
+def get_records(filename="lab4/output/sequences.gb"):
+    if not os.path.exists(filename):
+        filename = "output/sequences.gb"
+    return list(SeqIO.parse(filename, "genbank"))
