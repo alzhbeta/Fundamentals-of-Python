48d209776e05b1471d0660a5cdcab73fce06706e
alzhbeta <elizabethbaliuk@gmail.com>
2026-05-28 01:28:21 +0300

    Update translate_sequences.py

--- lab4/modules/translate_sequences.py
+++ lab4/modules/translate_sequences.py
@@ -1,1 +1,19 @@
+def extract_and_translate(records):
+    results = []
+    for record in records:
+        for feature in record.features:
+            if feature.type == "CDS":
+                location = feature.location
+                strand = "+" if location.strand >= 0 else "-"
+                cds_seq = feature.extract(record.seq)
+                protein_seq = cds_seq.translate(to_stop=True)
+                
+                results.append({
+                    "id": record.id,
+                    "desc": record.description,
+                    "location": f"[{location.start}:{location.end}]",
+                    "strand": strand,
+                    "translation": str(protein_seq)
+                })
+    return results
 
