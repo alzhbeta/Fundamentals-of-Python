e9d75086aefa2520473fbbeed268ad262a0b502f
alzhbeta <elizabethbaliuk@gmail.com>
2026-05-27 18:18:47 +0300

    Update test_main.py

--- lab3/test_main.py
+++ lab3/test_main.py
@@ -1,9 +1,10 @@
 import os
 
 def test_all_lab3_plots_exist():
+    
     import lab3.main
     
-    assert os.path.exists('lab3/iris_scatter.png')
-    assert os.path.exists('lab3/co2_dynamics.png')
-    assert os.path.exists('lab3/wine_scatter.png')
-    assert os.path.exists('lab3/elnino_dynamics.png')
+    assert os.path.exists('iris_scatter.png')
+    assert os.path.exists('co2_dynamics.png')
+    assert os.path.exists('wine_scatter.png')
+    assert os.path.exists('elnino_dynamics.png')
